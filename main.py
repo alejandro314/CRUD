e7a9dcaf9c8c06e944836f336f2d950f7a5d5311
alejandro314 <manuelmartines385@gmail.com>
2025-05-15 20:13:19 -0500

    actualizacion de la clase crud 15/05/2025

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 usuarios =[]
 
-
+id_contador =1
 
 
 @app.route("/", methods=["GET","POST"])
 def crud():
-    nombre = request.form.get("nombre")
-    email = request.form.get("email") 
-    usuarios.append({"username":nombre,"email":email})
+    global id_contador
 
-    return render_template("registro.html", usuarios = usuarios)
+    if request.method=="POST": #si accedemos a la ruta con datos en el formulario
+        nombre = request.form.get("nombre")
+        email = request.form.get("email") 
+        usuarios.append({"id":id_contador,"username":nombre,"email":email})#insertando usuario
+        id_contador+=1
+        return redirect(url_for("crud"))
+
+    id_eliminar=request.args.get("borrar") #siempre queda como texto
+    if id_eliminar:  #si me entregan una id a eliminar
+        #TODO: Eliminar el usuario con el id del parametro de la lista
+        for item in usuarios:
+            if str(item['id'])==id_eliminar:
+                usuarios.remove(item)
+                break
+        return redirect(url_for("crud"))  #llamar al nombre de la funcion
+    
+
+
+    return render_template("registro.html", usuarios = usuarios) #lista que entregamos al html
 
 
 
